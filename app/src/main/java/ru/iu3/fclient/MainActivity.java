27007980d5295a4be548f5bad0ad1caa385fb838
kykk0 <kykkozy@gmail.com>
2024-03-28 23:09:46 +0300

    done lab 4 without backend

--- app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -8,24 +8,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.icu.text.DecimalFormat;
-import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 
-import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements TransactionEvents{
+public class MainActivity extends AppCompatActivity implements TransactionEvents {
 
-
-    // Used to load the 'fclient' library on application startup.
     static {
         System.loadLibrary("fclient");
         System.loadLibrary("mbedcrypto");
@@ -33,16 +35,21 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
 
     private ActivityMainBinding binding;
     private String pin;
+
     public native String stringFromJNI();
+
     public static native int initRng();
+
     public static native byte[] randomBytes(int no);
-    public static native byte[] encrypt(byte[] key,byte[] data);
+
+    public static native byte[] encrypt(byte[] key, byte[] data);
+
     public static native byte[] decrypt(byte[] key, byte[] data);
+
     public native boolean transaction(byte[] trd);
 
 
     ActivityResultLauncher activityResultLauncher;
-
 
 
     @Override
@@ -54,20 +61,16 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
         int res = initRng();
         byte[] v = randomBytes(10);
 
-        // Example of a call to a native method
         TextView tv = binding.sampleButton;
         tv.setText(stringFromJNI());
 
-        activityResultLauncher  = registerForActivityResult(
+        activityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>(){
+                new ActivityResultCallback<ActivityResult>() {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == Activity.RESULT_OK) {
                             Intent data = result.getData();
-                            // обработка результата
-                            //String pin = data.getStringExtra("pin");
-                            //Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
                             pin = data.getStringExtra("pin");
                             synchronized (MainActivity.this) {
                                 MainActivity.this.notifyAll();
@@ -77,48 +80,49 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
                 });
     }
 
-    public void onButtonClick(View v)
-    {
-//        byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
-//        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
-//        byte[] dec = decrypt(key, enc);
-//        String s = new String(Hex.encodeHex(dec)).toUpperCase();
-//        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
-
-//        Intent it = new Intent(this, PinpadActivity.class);
-//        startActivity(it);
-
-        // print PinCode after like push notification
-
-//        Intent it = new Intent(this, PinpadActivity.class);
-//        startActivity(it);
-//        activityResultLauncher.launch(it);
-
-//
-//        new Thread(()-> {
-//            try {
-//                byte[] trd = stringToHex("9F0206000000000100");
-//                boolean ok = transaction(trd);
-//                runOnUiThread(()-> {
-//                    Toast.makeText(MainActivity.this, ok ? "ok" : "failed", Toast.LENGTH_SHORT).show();
-//                });
-//
-//            } catch (Exception ex) {
-//                // todo: log error
-//            }
-//        }).start();
-
-        byte[] trd = stringToHex("9F0206000000000100");
-        boolean ok = transaction(trd);
+    public void onButtonClick(View v) {
+//        byte[] trd = stringToHex("9F0206000000000100");
+//        boolean ok = transaction(trd);
+        testHttpClient();
     }
 
     @Override
     public void transactionResult(boolean result) {
-        runOnUiThread(()-> {
+        runOnUiThread(() -> {
             Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
         });
     }
 
+    protected void testHttpClient() {
+        new Thread(() -> {
+            try {
+                HttpURLConnection uc = (HttpURLConnection)
+                        (new URL("http://10.0.2.2:8081/api/v1/title").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() ->
+                {
+                    Toast.makeText(this, title, Toast.LENGTH_LONG).show();
+                });
+
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
+    }
+
+    protected String getPageTitle(String html)
+    {
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(html);
+        String p;
+        if (matcher.find())
+            p = matcher.group(1);
+        else
+            p = "Not found";
+        return p;
+    }
 
 
     @Override
@@ -138,15 +142,11 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
         return pin;
     }
 
-    public static byte[] stringToHex(String s)
-    {
+    public static byte[] stringToHex(String s) {
         byte[] hex;
-        try
-        {
+        try {
             hex = Hex.decodeHex(s.toCharArray());
-        }
-        catch (DecoderException ex)
-        {
+        } catch (DecoderException ex) {
             hex = null;
         }
         return hex;
